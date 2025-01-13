810317d1de1741c992ca73a9da0eef7a2c9f338e
Curious-kid0 <sathwikshenoy907@gmail.com>
2025-01-14 01:27:47 +0530

    created job opportunities by rendering to website

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+JOBS = [
+    {
+        'id' : 1 ,
+        'title' : 'Data Analyst' ,
+        'location' : 'Bengaluru,India',
+        'salary' : '10,00,000'
+    },
+    {
+        'id' : 2 ,
+        'title' : 'Data Scientist' ,
+        'location' : 'Delhi,India',
+        'salary' : '15,00,000'
+    },
+    {
+        'id' : 3 ,
+        'title' : 'Backend Developer' ,
+        'location' : 'Pune,India',
+        'salary' : '6,00,000'
+    },
+    {
+        'id' : 4 ,
+        'title' : 'Full Stack' ,
+        'location' : 'Remote',
+        'salary' : '8,00,000'
+    }
+]
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS)
 if __name__=="__main__" :
    app.run(host= '0.0.0.0', debug = True)
